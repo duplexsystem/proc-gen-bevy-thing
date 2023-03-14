b4478dde20fced16a7a18151becebc5e7f03802c
Zoë Gidiere <duplexsys@protonmail.com>
2023-03-14 15:54:45 -0500

    work

--- crates/lib/cpu/src/lib.rs
+++ crates/lib/cpu/src/lib.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::{App, Camera3dBundle, Commands, PluginGroup, Transform, Vec3};
+use bevy::render::RenderPlugin;
+use bevy::DefaultPlugins;
+use bevy_rapier3d::prelude::{NoUserData, RapierDebugRenderPlugin, RapierPhysicsPlugin};
+use bevy_rust_gpu::RustGpuPlugin;
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(
+            // Configure the render plugin with RustGpuPlugin's recommended WgpuSettings
+            RenderPlugin {
+                wgpu_settings: RustGpuPlugin::wgpu_settings(),
+            },
+        ))
+        .add_plugin(RustGpuPlugin)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .run();
 }
